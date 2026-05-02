7800574a0787546ac2025096726c1070d3daf091
扭矩不足 <1214113371@qq.com>
2026-05-02 13:33:56 +0800

    编译修复

---
+++ LayerBase.Generator/LayerBase.Generator/IsExternalInit.cs
@@ -0,0 +1,11 @@
+namespace System.Runtime.CompilerServices;
+
+#if !NET5_0_OR_GREATER
+/// <summary>
+/// Reserved to be used by the compiler for tracking metadata. 
+/// This class should not be used by developers in source code.
+/// </summary>
+internal static class IsExternalInit
+{
+}
+#endif
