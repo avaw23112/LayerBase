6ac5b47c42121a85a5cbfdf1ad7c0088b60eb8c4
扭矩不足 <1214113371@qq.com>
2026-05-10 17:37:46 +0800

    测试补全

--- LayerBase.Test/ActorLifecycleTests.cs
+++ LayerBase.Test/ActorLifecycleTests.cs
@@ -161,7 +161,7 @@ public class ActorLifecycleTests
 
         Assert.That(world.DestroyActor(oldId), Is.True);
         Assert.That(world.IsAlive(oldId), Is.False);
-        Assert.That(world.PostTo(oldId, new ActorLifecycleEvent(8)).IsSuccess, Is.True);
+        Assert.That(world.PostTo(oldId, new ActorLifecycleEvent(8)).IsSuccess, Is.False);
 
         ActorQueryResult query = world.QueryActor<ActorLifecycleEvent>();
         Assert.That(query.DebugActors, Is.Empty);
